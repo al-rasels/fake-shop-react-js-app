b36c67496ebcc5bdd5d6874a5ad9356b9e1b2038
shant0786 <rm.shanto786@gmail.com>
2024-10-24 20:39:37 +0600

    impv:Minor improvements

--- src/components/product-tile/index.jsx
+++ src/components/product-tile/index.jsx
@@ -10,8 +10,8 @@ function ProductTile({ product }) {
     dispatch(removeFromCart(product.id));
   }
   return (
-    <article className="flex bg-white flex-col items-center gap-3 mt-10 ml-5 rounded-xl px-2 py-3 shadow-sm hover:border-gray-200 hover:border-2 hover:transform hover:scale-105 duration-300 w-80">
-      <div className="h-[150px] relative flex items-end overflow-hidden rounded-xl object-cover ">
+    <article className="flex bg-white flex-col items-center gap-3 my-3 mx-2 rounded-xl px-2 py-3 shadow-sm hover:border-gray-200 hover:border-2 hover:transform hover:scale-105 duration-300 w-80 h-72">
+      <div className="h-[150px] relative flex items-end overflow-hidden rounded-xl object-cover">
         <img
           src={product.image}
           alt={product.name}
@@ -21,8 +21,10 @@ function ProductTile({ product }) {
 
       <h3 className="text-slate-700 font-sm mx-3">{product.title}</h3>
       <div>
-        <div class="my-3 flex items-center justify-between gap-5">
-          <p class="text-lg font-bold text-blue-500">{product.price} ৳</p>
+        <div class="flex align-center justify-between gap-3">
+          <div class="text-lg font-bold text-blue-500 block">
+            $ {product.price}
+          </div>
 
           <button
             onClick={
@@ -30,8 +32,10 @@ function ProductTile({ product }) {
                 ? handleRemoveFromCart
                 : handleAddToCart
             }
-            className="text-sm space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
-            {cart.some((item) => item.id === product.id) ? "Added" : "Add"}
+            className="text-sm rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
+            {cart.some((item) => item.id === product.id)
+              ? "Added to cart"
+              : "Add to cart"}
           </button>
         </div>
       </div>
