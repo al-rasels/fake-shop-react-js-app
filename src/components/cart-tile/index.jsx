5d8a901d28d36fe11dcbfa6c269b9087ac1712d7
shant0786 <rm.shanto786@gmail.com>
2024-10-26 01:04:18 +0600

    style:improved styling

--- src/components/cart-tile/index.jsx
+++ src/components/cart-tile/index.jsx
@@ -8,8 +8,8 @@ function CartTile({ cartItem }) {
   }
   return (
     <>
-      <div className="flex items-center max-w-lg py-3 px-5 bg-white shadow-sm mt-2 mb-2 ms-4 justify-center rounded-xl text-gray-900">
-        <div className="flex p-3">
+      <div className="flex items-center py-3 px-5 bg-white shadow-sm mt-2 mb-2 ms-4 justify-center rounded-xl text-gray-900">
+        <div className="flex">
           <div className="max-w-20 max-h-20 px-2">
             <img src={cartItem.image} alt="{cartItem.titile}" />
           </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -30,7 +30,7 @@ function Home() {
         </div>
       ) : (
         <>
-          <div className="bg-blue-50 backdrop-blur-xl rounded-lg grid sm:grid-cols-2 md:grid-cols-3 lg:grid-col-4 max-w-6xl mx-auto px-auto">
+          <div className="bg-blue-50 mx-auto col-12 align-middle backdrop-blur-xl rounded-lg grid sm:grid-cols-2 md:grid-cols-3 lg:grid-col-4  max-w-6xl px-auto">
             {products && products.length
               ? products.map((productItems) => (
                   <ProductTile product={productItems} />
