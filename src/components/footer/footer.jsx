c572ea40d76ef65b41ed495c6ad496b7c7116a30
shant0786 <rm.shanto786@gmail.com>
2025-03-10 01:07:22 +0600

    finalize design

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -7,15 +8,15 @@ const Footer = () => {
         &copy; {new Date().getFullYear()} Your Company. All rights reserved.
       </p>
       <div className="flex justify-center space-x-4 mt-2">
-        <a href="#" className="hover:text-gray-400">
+        <Link to="/" className="hover:text-gray-400">
           Privacy Policy
-        </a>
-        <a href="#" className="hover:text-gray-400">
+        </Link>
+        <Link to="/" className="hover:text-gray-400">
           Terms of Service
-        </a>
-        <a href="#" className="hover:text-gray-400">
+        </Link>
+        <Link to="/" className="hover:text-gray-400">
           Contact
-        </a>
+        </Link>
       </div>
     </footer>
   );
